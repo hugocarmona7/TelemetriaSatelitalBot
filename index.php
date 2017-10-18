5dbb61cfaa64e01246308a9fd5d42337a7cef491
hugocarmona7 <hugo.carmona@digitalcomtech.com>
2017-10-17 20:31:27 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$botToken = "474434774:AAFz-BQVoGDtzCuHmDUTrTpBMWDZc1woOug";
+$botToken = "453169222:AAG339gvu-xhnbooqP4ig23LH3fhaBEWNi8";
 $website = "https://api.telegram.org/bot".$botToken;
 
 $update = file_get_contents('php://input');
@@ -12,10 +12,10 @@ $lastname = $update["message"]["from"]["last_name"];
 
 switch($message)
 {
-	case "ID":
+	case "/id":
 		funcionid($chatId);
 		break;
-	case "Soporte":
+	case "/soporte":
 		funcionsoporte($chatId);
 		break;	
 	default:
@@ -38,7 +38,7 @@ function funcionid($chatId)
 
 function funcionsoporte($chatId)
 {
-	$mensaje = "Soporte tecnico:%0Acontacto@telemetriasatelital.com";
+	$mensaje = "Si requieres soporte tecnico mandanos un mensaje a:%0Acontacto@telemetriasatelital.com";
 	enviarmensaje($chatId,$mensaje);
 }
 
@@ -50,9 +50,9 @@ function noentiendo($chatId)
 
 function menuprincipal($chatId)
 {
-	$message = "Hola, soy <b>Telemetria Satelital Bot</b> y te puedo indicar tu ID%0ALo necesitas para que te pueda enviar alertas.";
+	$message = "Hola, soy <b>Telemetria Satelital Bot</b> y te puedo indicar tu /id%0ALo necesitas para que te pueda enviar alertas.%0ASi requieres asistencia solicita /soporte";
 	$tecladoprincipal = '&reply_markup={"keyboard":[["ID"],["Soporte"]],"resize_keyboard":true}';
-	$url = $GLOBALS[website].'/sendmessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message.$tecladoprincipal;
+	$url = $GLOBALS[website].'/sendmessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message;
 	file_get_contents($url);
 }
 
